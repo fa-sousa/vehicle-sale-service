d6d47dbcde4d8efd2c0674cce4398845ab9f1326
fa-sousa <fsousa292@gmail.com>
2026-07-02 17:47:16 -0300

    feat: initial spring boot setup

---
+++ src/main/kotlin/com/fasousa/vehiclesaleservice/VehicleSaleServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fasousa.vehiclesaleservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VehicleSaleServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<VehicleSaleServiceApplication>(*args)
+}
