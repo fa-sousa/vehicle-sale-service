555ed79619a4256b1885255dfaa140b10fd3e44d
fa-sousa <fsousa292@gmail.com>
2026-07-12 22:42:15 -0300

    ci: fix Sonar project and isolate context test with H2

--- src/test/kotlin/com/fasousa/vehiclesaleservice/VehicleSaleServiceApplicationTests.kt
+++ src/test/kotlin/com/fasousa/vehiclesaleservice/VehicleSaleServiceApplicationTests.kt
@@ -1,10 +1,23 @@
 package com.fasousa.vehiclesaleservice
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import kotlin.test.Test
 
-@SpringBootTest
+@SpringBootTest(
+	properties = [
+		"spring.datasource.url=jdbc:h2:mem:vehicle_sale_service_test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.show-sql=false",
+		"spring.jpa.open-in-view=false",
+		"spring.sql.init.mode=never",
+		"server.port=0"
+	]
+)
 @ActiveProfiles("test")
 class VehicleSaleServiceApplicationTests {
 
@@ -12,3 +25,4 @@ class VehicleSaleServiceApplicationTests {
 	fun contextLoads() {
 	}
 }
+
